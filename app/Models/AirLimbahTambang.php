6d4fa31ae07baca111dc4bbe8158148c9fe981c2
Calvin76en <151889984+Calvin76en@users.noreply.github.com>
2025-05-16 00:52:58 +0700

    Update AirLimbahTambang.php

--- app/Models/AirLimbahTambang.php
+++ app/Models/AirLimbahTambang.php
@@ -19,26 +19,24 @@ class AirLimbahTambang extends Model
         'monitoring_id', 'location_id'
     ];
 
-<<<<<<< HEAD
+
     // Relasi ke Location
-=======
->>>>>>> b533741de0b6b313976441ac7d8e8944ed274e0e
+
     public function location()
     {
         return $this->belongsTo(Location::class, 'location_id');
     }
 
-<<<<<<< HEAD
+
     // Relasi ke MonitoringType
-=======
->>>>>>> b533741de0b6b313976441ac7d8e8944ed274e0e
+
     public function monitoringType()
     {
         return $this->belongsTo(MonitoringType::class, 'monitoring_id');
     }
-<<<<<<< HEAD
-}
-=======
+
 }
 
->>>>>>> b533741de0b6b313976441ac7d8e8944ed274e0e
+}
+
+
