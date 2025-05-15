3749232aa0c02f45b5e8d3fb5d418ee1367bda78
Calvin76en <151889984+Calvin76en@users.noreply.github.com>
2025-05-16 00:52:04 +0700

    Update MonitoringType.php

--- app/Models/MonitoringType.php
+++ app/Models/MonitoringType.php
@@ -19,12 +19,10 @@ class MonitoringType extends Model
     // Relasi ke tabel locations (one-to-many)
     public function locations()
     {
-<<<<<<< HEAD
         return $this->hasMany(Location::class, 'monitoring_id', 'monitoring_id');  // Relasi dengan monitoring_id
     }
 }
-=======
+
         return $this->hasMany(Location::class, 'monitoring_id', 'id');  // Ganti 'monitoring_id' menjadi 'id'
     }
 }
->>>>>>> b533741de0b6b313976441ac7d8e8944ed274e0e
